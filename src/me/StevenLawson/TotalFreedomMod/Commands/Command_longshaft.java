8dd65572ab4c11a30ae7895c43074e11098e0487
Tyler Wrenn <mcbeat257@gmail.com>
2015-01-17 22:55:21 -0500

    Hilarious Add-on To It All

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_longshaft.java
@@ -0,0 +1,66 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+/*
+
+  ____                 _                              _     _             
+ |  _ \    ___   ___  (_)   __ _   _ __     ___    __| |   | |__    _   _ 
+ | | | |  / _ \ / __| | |  / _` | | '_ \   / _ \  / _` |   | '_ \  | | | |
+ | |_| | |  __/ \__ \ | | | (_| | | | | | |  __/ | (_| |   | |_) | | |_| |
+ |____/   \___| |___/ |_|  \__, | |_| |_|  \___|  \__,_|   |_.__/   \__, |
+                           |___/                                    |___/ 
+
+
+  _             _                 _                                     _   _   ____  
+ | |_   _   _  | |   ___   _ __  | |__    _   _   _ __     ___   _ __  | | | | |  _ \ 
+ | __| | | | | | |  / _ \ | '__| | '_ \  | | | | | '_ \   / _ \ | '__| | |_| | | | | |
+ | |_  | |_| | | | |  __/ | |    | | | | | |_| | | |_) | |  __/ | |    |  _  | | |_| |
+  \__|  \__, | |_|  \___| |_|    |_| |_|  \__, | | .__/   \___| |_|    |_| |_| |____/ 
+        |___/                             |___/  |_|                                  
+
+
+
+*/
+
+import java.util.Arrays;
+import java.util.List;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.HorseInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "Allows people to pull out their long shaft.", usage = "/<command>", aliases = "longcock")
+public class Command_longshaft extends TFM_Command
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        PlayerInventory inv = sender_p.getInventory();
+        ItemStack LongShaft = new ItemStack(Material.STICK, 1);
+        for (Enchantment ench : Enchantment.values())
+            {
+                LongShaft.addUnsafeEnchantment(ench, 10000);
+            }
+        inv.addItem(LongShaft);
+        sender_p.sendMessage(ChatColor.GOLD + "Giving you a long, hard shaft.");
+        
+        return true;
+    }
+}
