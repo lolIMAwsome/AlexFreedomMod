5347675b3c15bb42d92828a75c683fcb93912a7d
Tyler Wrenn <mcbeat257@gmail.com>
2015-02-03 21:38:24 -0500

    Commit

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_fuck.java
@@ -0,0 +1,115 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+/*
+
+  ____                 _                              _     _             
+ |  _ \    ___   ___  (_)   __ _   _ __     ___    __| |   | |__    _   _ 
+ | | | |  / _ \ / __| | |  / _` | | '_ \   / _ \  / _` |   | '_ \  | | | |
+ | |_| | |  __/ \__ \ | | | (_| | | | | | |  __/ | (_| |   | |_) | | |_| |
+ |____/   \___| |___/ |_|  \__, | |_| |_|  \___|  \__,_|   |_.__/   \__, |
+                           |___/                                    |___/ 
+
+
+  _             _                 _                                     _   _   ____  
+ | |_   _   _  | |   ___   _ __  | |__    _   _   _ __     ___   _ __  | | | | |  _ \ 
+ | __| | | | | | |  / _ \ | '__| | '_ \  | | | | | '_ \   / _ \ | '__| | |_| | | | | |
+ | |_  | |_| | | | |  __/ | |    | | | | | |_| | | |_) | |  __/ | |    |  _  | | |_| |
+  \__|  \__, | |_|  \___| |_|    |_| |_|  \__, | | .__/   \___| |_|    |_| |_| |____/ 
+        |___/                             |___/  |_|                                  
+
+
+
+*/
+
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import me.confuser.barapi.BarAPI;
+import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "For assholes only", usage = "/<command> [playername] [reason]")
+public class Command_fuck extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length < 1)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+        
+        if (player == null)
+        {
+            playerMsg(TotalFreedomMod.PLAYER_NOT_FOUND);
+            return true;
+        }
+        else if (args.length > 1)
+        {
+            final String reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+            smite(player, reason);
+            return true;
+        }
+        else if (!player.getName().equals("me"))
+        {
+            server.dispatchCommand(sender_p, "handjob");
+        }
+        else
+        {
+            smite(player);
+        }
+
+        return true;
+    }
+
+    public static void smite(final Player player, final String reason)
+    {
+        TFM_Util.bcastMsg(String.format("%s has been fucked in the ass\n" + ChatColor.GOLD + "Reason: %s", player.getName(), reason), ChatColor.RED);
+        String full = String.format(ChatColor.RED + "%s has been fucked in the ass because %s", player.getName(), reason);
+        BarAPI.setMessage((full.length() <= 64 ? full : String.format("%s has been fucked in the ass", player.getName())), 10);
+
+        //Strike with lightning effect:
+        final Location targetPos = player.getLocation();
+        final World world = player.getWorld();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
+                world.strikeLightning(strike_pos);
+            }
+        }
+
+        //Kill:
+        player.setHealth(0.0);
+    }
+    
+    public static void smite(final Player player)
+    {
+        TFM_Util.bcastMsg(player.getName() + " has been fucked in the asshole.\nThey have thus been smitten!", ChatColor.RED);
+
+        //Strike with lightning effect:
+        final Location targetPos = player.getLocation();
+        final World world = player.getWorld();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
+                world.strikeLightning(strike_pos);
+            }
+        }
+
+        //Kill:
+        player.setHealth(0.0);
+    }
+}
