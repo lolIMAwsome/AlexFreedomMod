2298f8f141c08f0400521d1218fd590ff3384346
reuben4545 <theminecraftgaming3000@gmail.com>
2015-02-07 15:51:31 +0000

    Update Command_sexy.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_sexy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sexy.java
@@ -6,16 +6,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
-@CommandParameters(description = "_herobrian35_ is totally sexy!", usage = "/<command>")
+@CommandParameters(description = "reuben4545 is totally sexy!", usage = "/<command>")
 public class Command_sexy extends TFM_Command
 {
 
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender_p.chat ("I would have thought I was sexy, but im Ugxly... We all know who is sexy, _herobrian35_ sexy!");
-        playerMsg("_herorbian35_ is sexy! :O", ChatColor.GRAY);
-        playerMsg("Also _herobrian35_ is sexy :) and he's our lead dev!", ChatColor.GOLD);
+        sender_p.chat ("I would have thought I was sexy, but im Ugxly... We all know who is sexy, reuben4545 sexy!");
+        playerMsg("reuben4545 is sexy! :O", ChatColor.GRAY);
+        playerMsg("Also _herobrian35_ is sexy :) and he's our co-chief-dev!", ChatColor.GOLD);
         return true;  
     }
 }
